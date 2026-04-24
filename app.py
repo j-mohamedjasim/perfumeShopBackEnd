c1c4b51671b53b12d8cbfa107e28b0026324dc1a
Mohamed Jasim <167366935+j-mohamedjasim@users.noreply.github.com>
2026-04-24 21:17:59 +0100

    withDatas

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import mysql.connector
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 def get_db():
     return mysql.connector.connect(
-        host="",
-        user="",
-        password="",
-        database=""
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
 
-@app.get("/product/<id>")
-def get_product(id):
+@app.get("/product/<ids>")
+def get_product(ids):
+    id_list = ids.split(",")
     db = get_db()
     cursor = db.cursor(dictionary=True)
 
-    cursor.execute("SELECT * FROM products WHERE id = %s",(id))
-    product = cursor.fetchone()
+    query = f"SELECT * FROM products WHERE id IN ({','.join(['%s'] * len(id_list))})"
+
+    cursor.execute(query, id_list)
+    product = cursor.fetchall()
 
     cursor.close()
     db.close()
@@ -27,6 +31,14 @@ def get_product(id):
     if not product:
         return jsonify({"error": "Product not found"}), 404
     
-    return jsonify(product)
+    combined = {
+        "name": product[0]["name"],
+        "description": product[0]["description"],
+        "image": product[0]["image"],
+        "price": [products["price"] for products in product],
+        "gram": [products["gram"] for products in product],
+        "stock": [products["stock"] for products in product]
+    }
+    return jsonify(combined)
 
 app.run()
